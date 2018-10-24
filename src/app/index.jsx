767f1567abd3b5088e5d3d1944a27354d279e5e1
Christopher Gouldstone <cgouldstone@avameo.de>
2018-10-24 15:10:45 +0200

    rending and jsx added

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,1 +1,13 @@
-console.log('JS test');
+import React from 'react';
+import { render } from 'react-dom';
+
+let myName = "Christopher";
+
+let element = (
+    <h1>Hello {myName}</h1> 
+)
+
+render(
+    element,
+    document.getElementById('root')
+);
